d4e747af7499603fb63f4b2bcfaac279d22edeec
alejandropf3 <alejandropefa31@gmail.com>
2025-11-26 13:35:30 -0500

    modificamos el archivo index y el archivo ejercicio 4

--- ejercicio_4.js
+++ ejercicio_4.js
@@ -1,3 +1,18 @@
-import { procesar } from "./Modules/procesaImpuesto";
+import { calculaImpuesto } from "./Modules/calculaImpuesto.js"; //importo la funcion que calcula impuestos
+const procesarImpuestos = function(){ //funcion que procesa los impuestos
+    let cantidad_productos = parseInt(prompt("Ingrese la cantidad de productos a procesar: "));
+    let total_final = 0;
 
-procesar();
+    for(let i = 0; i < cantidad_productos; i++)
+    {
+        let valor = parseFloat(prompt(`Ingrese el valor del producto ${i + 1}: `));
+        let porcentaje = parseInt(prompt(`Ingrese el porcentaje de impuesto prara el producto ${i+1}:`))
+        let impuesto = calculaImpuesto(valor, porcentaje); //llamo a la funcion que calcula impuestos
+        let valor_con_impuesto = valor + impuesto;
+        total_final += valor_con_impuesto;
+    }
+
+    alert(`El valor total final con impuestos es: ${total_final}`);
+}
+
+procesarImpuestos(); //llamo a la funcion que procesa los impuestos
